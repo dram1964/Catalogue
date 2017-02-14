dbb30fed9b67440eb1ef306cd789636e2b6ab0c3
David Ramlakhan <dram1964@gmail.com>
2017-02-14 19:40:27 +0000

    Fixed Header/column alignment in upload_excel_system_columns script

--- util/upload_excel_system_columns.pl
+++ util/upload_excel_system_columns.pl
@@ -8,7 +8,7 @@ use Text::Iconv;
 
 my $converter = Text::Iconv->new("utf-8", "windows-1251");
 
-my $excel = Spreadsheet::XLSX->new('/home/dr00/Downloads/USQL2vDWH_Aardvark_live.xlsx', $converter);
+my $excel = Spreadsheet::XLSX->new('/home/dr00/Downloads/UWHSLIVE Metadata Dump.xlsx', $converter);
 
 my $hic = $excel->{Worksheet}->[0];
 my $system_name = $hic->{Name};
@@ -17,13 +17,7 @@ print "Loading data from $system_name\n";
 my ( $row_min, $row_max ) = $hic->row_range();
 my ( $col_min, $col_max ) = $hic->col_range();
 
-my @header;
-for my $col ( $col_min .. $col_max ) {
-    my $cell = $hic->get_cell( 0, $col );
-    next unless $cell;
-    push (@header, $cell->value());
-}
-print join(":", "Row:System Name", @header[0,1,2,3,7,8]), "\n";
+print join(":", qw/Row System Database Schema Table Column Type Size/), "\n";
 
 my $test_run = 1;
 &load_data($test_run);
@@ -39,16 +33,19 @@ sub load_data {
         	next unless $cell;
 			push (@record, $cell->value());
     	}
-    	$data = {'system_name' => $system_name,
-			'database_name' => $record[0],
-			'schema_name' => $record[1],
-			'table_name' => $record[2],
-			'column_name' => $record[3],
-			'column_type' => $record[7],
-			'column_size' => $record[8],
+    	$data = {'system_name' => $record[0],
+			'database_name' => $record[1],
+			'schema_name' => $record[2],
+			'table_name' => $record[3],
+			'column_name' => $record[4],
+			'column_type' => $record[8],
+			'column_size' => $record[9],
     	};
 		my $import = Catalogue::Systems::Importer->new($data);
-		print join(":", "($row of $row_max)", $system_name, @record[0,1,2,3,7,8]), "\n";
+		print join(":", "($row of $row_max)", $data->{system_name}, 
+		  $data->{database_name}, $data->{schema_name}, 
+		  $data->{table_name}, $data->{column_name}, 
+		  $data->{column_type}, $data->{column_size}), "\n";
 		$import->add_or_update_system unless $test_run;
 		$data = ();
 	}
