498f201758460a58fd088d5b01cadcb6175cc569
David Ramlakhan <dram1964@gmail.com>
2017-02-25 12:16:18 +0000

    Changed dataset explore to show piechart with dummy data in dashboard area

--- root/static/js/piechart.js
+++ root/static/js/piechart.js
@@ -1,22 +1,22 @@
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
-      function drawChart() {
+function drawChart() {
 
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
+var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Work',     11],
+  ['Eat',      2],
+  ['Commute',  2],
+  ['Watch TV', 2],
+  ['Sleep',    7]
+]);
 
-        var options = {
-          title: 'My Daily Activities'
-        };
+var options = {
+  title: 'dataset'
+};
 
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-        chart.draw(data, options);
-      }
+chart.draw(data, options);
+}
