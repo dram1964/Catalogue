d0b6681001e8afcc51c4363f31ad9d6014c8ef31
David Ramlakhan <dram1964@gmail.com>
2017-03-17 16:11:22 +0000

    Added template for Troponins to explore_json template

---
+++ root/static/js/json_tpts.js
@@ -0,0 +1,37 @@
+google.charts.load('current', {'packages':['corechart', 'controls']});
+google.charts.setOnLoadCallback(tptDashboard);
+
+function tptDashboard() {
+	var tptDashboard = new google.visualization.Dashboard(
+	    document.getElementById('tpt_dashboard_div'));
+	var tpt_data = google.visualization.arrayToDataTable(tptRanges);
+
+	var tptTypeFilter = new google.visualization.ControlWrapper({
+	  'controlType': 'CategoryFilter',
+	  'containerId': 'tpt_filter_div',
+	  'options' : {
+	    'filterColumnLabel': 'Range',
+	  }
+	});
+	var tptPieChart = new google.visualization.ChartWrapper({
+	  'chartType': 'PieChart',
+	  'containerId': 'tpt_piechart_div',
+	  'options': {
+            'is3D': false,
+	    'pieSliceText': 'percentage',
+	    'legend': 'none'
+	  }, 'view' : {'columns' : [0,1]}
+	});
+
+	var tptTableChart = new google.visualization.ChartWrapper({
+	  'chartType': 'Table',
+	  'containerId': 'tpt_tablechart_div',
+	  'options': {
+	  }, 'view' : {'columns' : [0, 1]}
+	});
+
+	tptDashboard.bind(tptTypeFilter, [tptPieChart, tptTableChart] );
+
+	tptDashboard.draw(tpt_data);
+
+}
