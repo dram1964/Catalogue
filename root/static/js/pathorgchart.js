7e477caacf229380a06ecb083350b61b8d486c94
David Ramlakhan <dram1964@gmail.com>
2017-03-24 18:26:41 +0000

    Added more detail to pathorgchart json

--- root/static/js/pathorgchart.js
+++ root/static/js/pathorgchart.js
@@ -6,17 +6,6 @@ function pathOrgChart() {
   data.addColumn('string', 'Parent');
   data.addColumn('string', 'ToolTip');
   
-//data.addRows([
-//  ['Sample', '', 'A specimen collected from a patient'],
-//  ['Sample Number', 'Sample', 'Unique identifier for the specimen'],
-//  ['Collect Date', 'Sample', 'When the sample was collected'],
-//  ['Order', 'Sample', 'What Pathology Tests were ordered'],
-//  ['Order Code', 'Order', 'Local Code for each order'],
-//  ['Test', 'Order Code', 'Analytes to be tested for'],
-//  ['Result', 'Test', 'Value obtained for Test'],
-//  ['Result Date', 'Test', 'Date Result value obtained']
-//]);
-
   data.addRows(orgchart.tree);
 
   var chart = new google.visualization.OrgChart(document.getElementById('path_org_chart'));
