d1f74516f449740b30e7a16c3758a5a382d70419
David Ramlakhan <dram1964@gmail.com>
2017-05-01 09:18:05 +0100

    Modified upload_excel_database_columnsto use new importer module

--- util/upload_excel_database_columns.pl
+++ util/upload_excel_database_columns.pl
@@ -1,61 +1,29 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
-use Getopt::Simple;
+use Getopt::Long;
 use Spreadsheet::XLSX;
 use Text::Iconv;
 
-use Catalogue::Systems::Importer;
+use Catalogue::Importer::Database;
 
-my($options) = {
-	help => {
-		type => '',
-		env => '-', 
-		default => '',
-		verbose => '',
-		order => 1,
-	},
-	server => {
-		type => '=s',
-		env => '-', 
-		default => '',
-		verbose => 'Specify the server name to be used during upload',
-		order => 2,
-	},
-	worksheet => {
-		type => '=i',
-		env => '-', 
-		default => '',
-		verbose => 'Specify the worksheet number starting from 0',
-		order => 3,
-	},
-	data_from => {
-		type => '=i',
-		env => '-', 
-		default => '',
-		verbose => 'Specify number of header rows to skip',
-		order => 4,
-	},
-};
+my $server_name;
+my $sheet_number = 0;
+my $first_data_row = 0;
+my $help;
 
-my($option) = Getopt::Simple->new();
+GetOptions("server=s" => \$server_name, 
+	"worksheet=i" => \$sheet_number, 
+	"data_from=i" => \$first_data_row,
+	"help" => \&help) or &help();
 
-if (! $option -> getOptions($options, "Usage: perl -Ilib util/upload_excel_database_columns.pl [options]"))
-{
-	exit(-1); 
-}
+&help() if ($help || !$server_name);
 
-
-
-my $server_name = $$option{'switch'}{'server'};
-my $sheet_number = $$option{'switch'}{'worksheet'};
-my $first_data_row = $$option{'switch'}{'data_from'};
-print "server: $server_name\n";
-print "worksheet: $sheet_number\n";
-print "skip rows: $first_data_row\n";
+my $filename = "data/$server_name.xlsx";
+die "Import file '$filename' not found\n" unless (-f $filename);
 
 my $converter = Text::Iconv->new("utf-8", "windows-1251");
-my $excel = Spreadsheet::XLSX->new("/home/dr00/Catalogue/data/$server_name.xlsx", $converter);
+my $excel = Spreadsheet::XLSX->new($filename, $converter);
 my $sheet = $excel->{Worksheet}->[$sheet_number];
 my ( $row_min, $row_max ) = $sheet->row_range();
 my ( $col_min, $col_max ) = $sheet->col_range();
@@ -84,7 +52,7 @@ sub load_data {
 			'column_type' => $record[7],
 			'column_size' => $record[8],
     	};
-		my $import = Catalogue::Systems::Importer->new($data);
+		my $import = Catalogue::Importer::Database->new($data);
 		print join(":", "($row of $row_max)", $import->server_name, 
 		  $import->database_name, $import->schema_name, 
 		  $import->table_name, $import->column_name, 
@@ -100,3 +68,17 @@ sub load_data {
 	print "Records Inserted\n" unless $test_run;
 	$test_run = 0;
 }
+
+sub help() {
+	print <<HELPTEXT;
+Usage: $0 -s servername -w 0 -d 0
+
+-s --server		Name of file to load without '.xlsx' extension
+                	Will be used as server name during upload
+-w --worksheet		Worksheet index. Begins at 0
+-d --data_from		Number of header rows to skip
+
+HELPTEXT
+	exit 0;
+}
+
