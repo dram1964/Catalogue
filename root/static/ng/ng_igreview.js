0ede77ff916f55e7fa25e8460ca4852ebf55b7d4
dram1964 <dram1964@gmail.com>
2017-09-27 10:37:20 +0000

    added a form and submit button to igadmin display template

--- root/static/ng/ng_igreview.js
+++ root/static/ng/ng_igreview.js
@@ -2,15 +2,19 @@ angular.module('igadmin', [])
   .controller('ScoreCard', [function() {
     var self = this;
     self.msg = "cooking with gas";
-    self.risks = [ {id: 0}
+    self.risks = [ {rating : {name : 'rating1'}, likely : {name : 'likely1'}}
     ];
 
     self.addRisk = function() {
-	self.risks.push({id : self.risks.length + 1}); 
+	var suffix = self.risks.length + 1;
+	self.risks.push({
+	rating : {name : 'rating' + suffix},
+	likely : {name : 'likely' + suffix} 
+	});
     };
-    self.updateScore = function() {
-	if (self.rating >= 1 && self.likely >= 1) {
-	    self.score = self.rating * self.likely;
+    self.updateScore = function(rating) {
+	if (self.likely >= 1) {
+	    self.score = rating * self.likely;
 	}
     };
 
